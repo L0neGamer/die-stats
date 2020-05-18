811d4c8b5c6695cf75d15811103e987a990fec9a
Benjamin <benjaminbwm@gmail.com>
2020-05-18 11:34:28 +0100

    removed an example

--- app/Main.hs
+++ app/Main.hs
@@ -13,8 +13,6 @@ main = do
     print $ stats charGen
     putStrLn "\nlevel 9 firebolt vs ac 15 (+4 mod)"
     print $ stats $ consAttack 9 15 (2.* d 10) 0
-    putStrLn "\n 'gatling lightning'"
-    print $ stats $ 4 .* consAttack 9 15 (2.*d 6) 0
     putStrLn "\nlevel 9 fighter maul 2 attacks vs ac 15 (+5 mod)"
     print $ stats $ 2 .* consAttack 9 15 (2.* d 6) 5
     putStrLn "\nlevel 9 fighter maul 2 attacks reroll 1s and 2s vs ac 15 (+5 mod)"
